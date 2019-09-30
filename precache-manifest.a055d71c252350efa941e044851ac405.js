20c274e6b39ce359c1c78f4e40e07858ace65d22
Tsarina <tsarina.islam@gmail.com>
2019-09-29 23:01:47 -0400

    Updates

---
+++ precache-manifest.a055d71c252350efa941e044851ac405.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd69529a1d79395d728321aaa2d4dfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc39f7cb250871423159",
+    "url": "/static/css/main.9953e079.chunk.css"
+  },
+  {
+    "revision": "02e06bbd53afafb4d708",
+    "url": "/static/js/2.0e865fa3.chunk.js"
+  },
+  {
+    "revision": "bc39f7cb250871423159",
+    "url": "/static/js/main.26674147.chunk.js"
+  },
+  {
+    "revision": "f6f66a419437be6657e9",
+    "url": "/static/js/runtime-main.2419d67d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
